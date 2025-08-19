cecf6a63adf7bbb5fd6da0768698c216495898f3
mrezabarus <mrezabarus87@gmail.com>
2025-08-20 00:51:55 +0700

    deploy nextjs ke render.com

--- lib/user/api.ts
+++ lib/user/api.ts
@@ -1,2 +1,2 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 //const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://erpfullstack.onrender.com/";
